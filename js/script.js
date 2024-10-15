8c89d3c940ab84f3e290e9e771924050207f5eac
joaopabezerra <paulonaativa2007@gmail.com>
2024-10-15 11:14:55 -0300

    a

--- js/script.js
+++ js/script.js
@@ -109,23 +109,4 @@ document.addEventListener('DOMContentLoaded', () => {
         let whatsappUrl = `https://wa.me/+5561993099538?text=${encodeURIComponent(message)}`;
         window.open(whatsappUrl, '_blank');
     });
-
-    // Novo código para enviar o pedido via WhatsApp ao clicar no botão "pede agora!"
-    document.querySelector('#submit-order').addEventListener('click', (event) => {
-        event.preventDefault();
-        
-        let nome = document.querySelector('input[placeholder="nome"]').value;
-        let sobrenome = document.querySelector('input[placeholder="sobrenome"]').value;
-        let email = document.querySelector('input[placeholder="email"]').value;
-        let telefone = document.querySelector('input[placeholder="telefone"]').value;
-        let endereco = document.querySelector('input[placeholder="Endereço"]').value;
-        let referencia = document.querySelector('input[placeholder="referencia"]').value;
-        let detalhes = document.querySelector('textarea').value;
-
-        let message = `Pedido de ${nome} ${sobrenome}:\nEmail: ${email}\nTelefone: ${telefone}\nEndereço: ${endereco}\nReferência: ${referencia}\nDetalhes do pedido:\n${detalhes}`;
-        
-        let whatsappUrl = `https://wa.me/+5561993099538?text=${encodeURIComponent(message)}`;
-        window.open(whatsappUrl, '_blank');
-    });
 });
-
